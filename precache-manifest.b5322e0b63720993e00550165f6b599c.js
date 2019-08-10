eba664eb82adf92836da656188e4815af324152e
Rajiv Venugopal <rajivgopal@yahoo.com>
2019-08-10 10:47:43 +0800

    Updates

---
+++ precache-manifest.b5322e0b63720993e00550165f6b599c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0ff2d2cd6b278f4ff217cc58cfecb9c",
+    "url": "/react-state-management-tutorial/index.html"
+  },
+  {
+    "revision": "0163278a960495312424",
+    "url": "/react-state-management-tutorial/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "10bf244ca692441702db",
+    "url": "/react-state-management-tutorial/static/js/2.1b3a0643.chunk.js"
+  },
+  {
+    "revision": "0163278a960495312424",
+    "url": "/react-state-management-tutorial/static/js/main.5e5d135f.chunk.js"
+  },
+  {
+    "revision": "f03cb261689636763f3b",
+    "url": "/react-state-management-tutorial/static/js/runtime~main.5472ccb1.js"
+  }
+]);
